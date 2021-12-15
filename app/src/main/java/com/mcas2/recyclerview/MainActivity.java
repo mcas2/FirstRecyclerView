b79ec76f8616ee11f41f29502efff4fad46ddac0
mcas2 <mcas2@tutanota.com>
2021-12-15 09:48:55 +0100

    Debería funcionar

--- app/src/main/java/com/mcas2/recyclerview/MainActivity.java
+++ app/src/main/java/com/mcas2/recyclerview/MainActivity.java
@@ -1,14 +1,56 @@
 package com.mcas2.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> lDivisas;
+    private ArrayList<Double> facCambio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fillDivisas();
+        fillCambios();
+
+        RecyclerView rvDivisas = findViewById(R.id.rvDivisas);
+        LinearLayoutManager managerLayout = new LinearLayoutManager(this);
+        rvDivisas.setLayoutManager(managerLayout);
+        MiAdaptador adaptador = new MiAdaptador(this, lDivisas);
+        rvDivisas.setAdapter(adaptador);
+    }
+
+    private void fillDivisas (){
+        lDivisas.add("USD");
+        lDivisas.add("GBP");
+        lDivisas.add("CAD");
+        lDivisas.add("AUD");
+        lDivisas.add("JPY");
+        lDivisas.add("INR");
+        lDivisas.add("NZD");
+        lDivisas.add("CHF");
+        lDivisas.add("ZAR");
+        lDivisas.add("RUB");
+    }
+
+    private void fillCambios (){
+        facCambio.add(1.1293946);
+        facCambio.add(0.85447758);
+        facCambio.add(1.4339265);
+        facCambio.add(1.5788175);
+        facCambio.add(128.17384);
+        facCambio.add(85.36992);
+        facCambio.add(1.6631981);
+        facCambio.add(1.0441295);
+        facCambio.add(18.030472);
+        facCambio.add(83.219626);
     }
 }
--- app/src/main/java/com/mcas2/recyclerview/MiAdaptador.java
+++ app/src/main/java/com/mcas2/recyclerview/MiAdaptador.java
@@ -1,11 +1,13 @@
 package com.mcas2.recyclerview;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ConcatAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -13,30 +15,44 @@ import java.util.ArrayList;
 public class MiAdaptador extends RecyclerView.Adapter<MiAdaptador.ViewHolder> {
 
     private ArrayList<String> lDivisas;
-    private LayoutInflater mInflater;
+    private LayoutInflater inflater;
+
+    MiAdaptador(Context context, ArrayList<String> lDivisas){
+        this.lDivisas = lDivisas;
+        this.inflater = LayoutInflater.from(context);
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.row, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String divisa = lDivisas.get(position);
+        holder.tvDivisa.setText(divisa);
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lDivisas.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         TextView tvDivisa;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            tvDivisa = itemView.findViewById(R.id.tvDivisa);
+        public ViewHolder(@NonNull View item) {
+            super(item);
+            tvDivisa = item.findViewById(R.id.tvDivisa);
         }
     }
 
